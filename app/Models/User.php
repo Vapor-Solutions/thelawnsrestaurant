3c80089e4613fda17febf4a3681a7daba32f5654
Steve Nyanumba <reach@stevenyanumba.com>
2022-05-11 16:51:19 +0300

    Corrected the DB Relationships in the users model

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,6 +64,6 @@ class User extends Authenticatable
 
     public function roles()
     {
-        return $this->belongsToMany(Role::class, 'role_user_table', 'user_id', 'role_id');
+        return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
     }
 }
