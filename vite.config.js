430d6a40e64f2dd7bcc33910e1917402f45813bc
Steve Nyanumba <reach@stevenyanumba.com>
2023-03-19 19:06:33 +0300

    The Backbone is complete

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
+                'resources/sass/front.scss',
                 'resources/js/front.js',
                 'resources/js/app.js',
             ],
